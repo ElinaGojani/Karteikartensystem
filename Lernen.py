ccc1e2dde0c9f9ef34d8263eeb397cba38f518ee
ArthurFleck35x <152798623+ArthurFleck35x@users.noreply.github.com>
2024-02-20 16:48:51 +0100

    Update Lernen.py

--- Lernen.py
+++ Lernen.py
@@ -3,10 +3,6 @@ import tkinter.font as font
 import subprocess
 
 
-# Ikon hinzufügen (auskommentiert)
-
-
-
 class FragenAntwortenApp:
     def __init__(self, master):
         self.master = master
@@ -21,18 +17,23 @@ class FragenAntwortenApp:
         self.frage_label = tk.Label(master, text="Frage:", bg="olive")
         self.frage_label.grid(row=0, column=0, padx=10, pady=10)
 
-        self.frage_text = tk.Label(master, text=self.frage_antwort_liste[self.aktuelle_frage_index], height=5, width=30, bg='darkgrey', fg='white', borderwidth=2, relief="sunken")
+        self.frage_text = tk.Label(master, text=self.frage_antwort_liste[self.aktuelle_frage_index], height=5, width=30,
+                                   bg='darkgrey', fg='white', borderwidth=2, relief="sunken")
         self.frage_text.grid(row=0, column=1, padx=10, pady=10)
 
-        self.antwort_button = tk.Button(master, text="Antwort", bg='darkgrey', fg='white', height=2, width=25,font = self.myFont, command=self.zeige_antwort)
+        self.antwort_button = tk.Button(master, text="Antwort", bg='darkgrey', fg='white', height=2, width=25,
+                                        font=self.myFont, command=self.zeige_antwort)
         self.antwort_button.grid(row=1, column=1, padx=10, pady=10)
-        self.ZumForumButton= tk.Button(master, text='Zum Forum', bg='darkgrey', fg='white', height=2, width=12, font = self.myFont, command=self.ClickForum).grid(row=0, column=3)
-        self.naechste_button = tk.Button(master, text="Nächste Frage",  bg='darkgrey', fg='white', height=2, width=12, font = self.myFont, command=self.naechste_frage)
+        self.ZumForumButton = tk.Button(master, text='Zum Forum', bg='darkgrey', fg='white', height=2, width=12,
+                                        font=self.myFont, command=self.ClickForum).grid(row=0, column=3)
+        self.naechste_button = tk.Button(master, text="Nächste Frage", bg='darkgrey', fg='white', height=2, width=12,
+                                         font=self.myFont, command=self.naechste_frage)
         self.naechste_button.grid(row=1, column=3, padx=10, pady=10)
 
     def ClickForum(self):
         subprocess.Popen(["python", "Forumseite.py"])
         self.master.destroy()
+
     def lade_fragen_antworten(self):
         try:
             with open("datenbank.txt", "r", encoding="utf-8") as file:
@@ -66,4 +67,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = FragenAntwortenApp(root)
     root.mainloop()
-
